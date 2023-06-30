9de4c813d626019f061950962019c873258cbc35
Aziz <aziznal.dev@gmail.com>
2023-06-30 22:58:47 +0300

    add test page with different sidenav

--- app/(split-layout)/layout.tsx
+++ app/(split-layout)/layout.tsx
@@ -1,4 +1,5 @@
 import Sidenav from "@/components/sidenav";
+import Link from "next/link";
 
 export default function Layout_SplitLayout({
   children,
@@ -9,7 +10,14 @@ export default function Layout_SplitLayout({
     <div className="relative flex min-h-full">
       <Sidenav className="sticky top-0" />
 
-      <main className="flex flex-1 flex-col">{children}</main>
+      <main className="flex flex-1 flex-col">
+        <div className="flex gap-4">
+          <Link href="/home">Home</Link>
+          <Link href="/test">Test</Link>
+        </div>
+
+        {children}
+      </main>
     </div>
   );
 }
--- app/(split-layout)/test/page.tsx
+++ app/(split-layout)/test/page.tsx
@@ -3,17 +3,17 @@
 import { useSidenavContext } from "@/contexts/sidenav-context";
 import { createPortal } from "react-dom";
 
-export default function Home() {
+export default function Test() {
   const { getPortalContainer, portalReady } = useSidenavContext();
 
   if (!portalReady) return null;
 
   return (
     <>
-      {createPortal(<h1>Home Sidenav</h1>, getPortalContainer())}
+      {createPortal(<h1>Test Sidenav</h1>, getPortalContainer())}
 
       <h1 className="flex h-full items-center justify-center gap-4 text-4xl">
-        Home Page
+        Test Page
       </h1>
     </>
   );
